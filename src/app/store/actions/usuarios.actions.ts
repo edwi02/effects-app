7324d55e48d4735d7a30a1a16dc5a09ae80404d6
Edwin Quintero R <edwi02@gmail.com>
2021-02-13 11:22:02 -0500

    Actions creada para los usuarios

---
+++ src/app/store/actions/usuarios.actions.ts
@@ -0,0 +1,14 @@
+import { Usuario } from './../../models/usuario.model';
+import { createAction, props } from '@ngrx/store';
+
+export const cargarUsuarios = createAction('[Usuarios] Cargar Usuarios');
+
+export const cargarUsuariosSuccess = createAction(
+    '[Usuarios] Cargar Usuarios Success',
+    props<{ usuarios: Usuario[]}>()
+);
+
+export const cargarUsuariosError = createAction(
+    '[Usuarios] Cargar Usuarios Success',
+    props<{ payload: any }>()
+);
