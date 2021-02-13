ac1f6d7579685f159cf72f1cbf0773feb6f72980
Edwin Quintero R <edwi02@gmail.com>
2021-02-13 11:38:54 -0500

    Reducer creado para usuarios

---
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -0,0 +1,40 @@
+import { createReducer, on } from '@ngrx/store';
+import { cargarUsuarios, cargarUsuariosSuccess, cargarUsuariosError } from '../actions';
+
+
+export interface UsuariosState {
+    user: any[];
+    loaded: boolean;
+    loading: boolean;
+    error: any;
+}
+
+export const usuariosInitialState: UsuariosState = {
+    user: [],
+    loaded: false,
+    loading: false,
+    error: null,
+};
+
+// tslint:disable-next-line:variable-name
+const _usersReducer = createReducer(usuariosInitialState,
+
+    on( cargarUsuarios , state => ({ ...state, loading: true })),
+    on( cargarUsuariosSuccess , (state, { usuarios }) => ({
+        ...state,
+        loading: false,
+        loaded: true,
+        users: [ ...usuarios ],
+    })),
+    on( cargarUsuariosError , (state, { payload }) => ({
+        ...state,
+        loading: false,
+        loaded: false,
+        error: payload,
+    })),
+
+);
+
+export function usersReducer(state, action): any {
+    return _usersReducer(state, action);
+}
